f2ba80d897d0d15d3531646b3321b3733758d301
Богдан Балушкин <bogdan.balushkin5@gmail.com>
2022-09-07 20:39:40 +0300

    Complete task 1.2

--- 1_concepts/1_2_box_pin/src/main.rs
+++ 1_concepts/1_2_box_pin/src/main.rs
@@ -1,4 +1,92 @@
-fn main()
+use std::pin::Pin;
+
+trait MutMeSomehow {
+  fn mut_me_somehow( self: Pin< &mut Self > );
+}
+
+trait SayHi: std::fmt::Debug {
+  fn say_hi(self: Pin<&Self>) {
+      println!("Hi from {:?}", self)
+  }
+}
+
+impl< T : Default > MutMeSomehow for Box< T >
 {
-  println!( "Implement me!" );
+  fn mut_me_somehow( mut self: Pin< &mut Self > ) {
+    self.set( Box::new( T::default() ) );
+  }
+}
+
+impl< T : Default > MutMeSomehow for Vec< T >
+{
+  fn mut_me_somehow( mut self: Pin< &mut Self > ) 
+  {
+    self.set( vec![ T::default() ] );
+  }
+}
+
+impl MutMeSomehow for String
+{
+  fn mut_me_somehow( mut self: Pin< &mut Self > ) 
+  {
+    self.set( String::from( "Hello" ) );
+  }
+}
+
+impl< T : std::fmt::Debug > SayHi for Box< T > {}
+impl< T : std::fmt::Debug > SayHi for Vec< T > {}
+impl SayHi for String {}
+
+
+fn main() {}
+
+#[ cfg( test )]
+mod tests
+{
+  use super::*;
+
+  #[ test ]
+  fn mut_pinned_box()
+  {
+    let mut data = Box::new( "Hi" );
+    Pin::new( &mut data ).mut_me_somehow();
+    assert_eq!( *data, "" );
+  }
+
+  #[ test ]
+  fn mut_pinned_vec()
+  {
+    let mut data = vec![ 1, 2, 3];
+    Pin::new( &mut data ).mut_me_somehow();
+    assert_eq!( data, vec![ 0 ] );
+  }
+
+  #[ test ]
+  fn mut_pinned_string()
+  {
+    let mut data = String::from( "World" );
+    Pin::new( &mut data ).mut_me_somehow();
+    assert_eq!( &data, "Hello" );
+  }
+
+  #[ test ]
+  fn say_hi_box()
+  {
+    let data = Box::new( "Hi" );
+    Pin::new( &data ).say_hi();
+  }
+
+  #[ test ]
+  fn say_hi_vec()
+  {
+    let data = vec![ 1, 2, 3];
+    Pin::new( &data ).say_hi();
+  }
+
+  #[ test ]
+  fn say_hi_string()
+  {
+    let data = String::new();
+    Pin::new( &data ).say_hi();
+  }
 }
