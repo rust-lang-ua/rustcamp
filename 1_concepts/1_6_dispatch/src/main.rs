5cb51fc2146864a5043a09d506581f98b77bd3c6
Богдан Балушкин <bogdan.balushkin5@gmail.com>
2022-09-13 17:54:58 +0300

    Done 1.7

--- 1_concepts/1_6_dispatch/src/main.rs
+++ 1_concepts/1_6_dispatch/src/main.rs
@@ -1,4 +1,185 @@
-fn main()
+use std::{ borrow::Cow, collections::HashMap };
+
+pub trait Storage< K, V >
 {
-  println!( "Implement me!" );
+  fn set( &mut self, key : K, val : V );
+  fn get( &self, key : &K ) -> Option< &V >;
+  fn remove( &mut self, key : &K ) -> Option< V >;
+}
+
+#[ derive( Debug ) ]
+pub struct CanNotAddUser;
+
+pub trait UserRepository
+{
+  fn add( &mut self, user : User ) -> Result< (), CanNotAddUser >;
+  fn get( &self, key : u64 ) -> Option< &User >;
+  fn update( &mut self, user : User );
+  fn remove( &mut self, key : u64 ) -> Option< User >;
+}
+
+#[ derive( Debug, Default, Clone, PartialEq ) ]
+pub struct User
+{
+  pub id : u64,
+  pub email : Cow< 'static, str >,
+  pub activated : bool,
+}
+
+#[ derive( Default ) ]
+pub struct DB< K, V >
+{
+  storage : HashMap< K, V >,
+}
+
+impl< K : Eq + std::hash::Hash, V > Storage< K, V > for DB< K, V >
+{
+  fn set( &mut self, key : K, val : V )
+  {
+    self.storage.insert( key, val );
+  }
+
+  fn get( &self, key : &K ) -> Option< &V > {
+    self.storage.get( key )
+  }
+
+  fn remove( &mut self, key : &K ) -> Option< V > {
+    self.storage.remove( key )
+  }
+}
+
+pub struct UserRepositoryStatic< S : Storage< u64, User > >
+{
+  storage : S
+}
+
+impl< S > UserRepositoryStatic< S >
+where
+  S : Storage< u64, User >
+{
+  pub fn new( storage : S ) -> Self
+  {
+    Self { storage }
+  }
+}
+
+impl< S > UserRepository for UserRepositoryStatic< S >
+where
+  S : Storage< u64, User >
+{
+  fn add( &mut self, user : User ) -> Result< (), CanNotAddUser >
+  {
+    if self.storage.get( &user.id ).is_some()
+    {
+      return Err( CanNotAddUser );
+    }
+    Ok( self.storage.set( user.id, user ) )
+  }
+
+  fn get( &self, key : u64 ) -> Option< &User >
+  {
+    self.storage.get( &key )
+  }
+
+  fn update( &mut self, user : User )
+  {
+    self.storage.set( user.id, user )
+  }
+
+  fn remove( &mut self, key : u64 ) -> Option< User >
+  {
+    self.storage.remove( &key )
+  }
+}
+
+pub struct UserRepositoryDynamic
+{
+  storage : Box< dyn Storage< u64, User > >
+}
+
+impl UserRepositoryDynamic
+{
+  pub fn new( storage : Box< dyn Storage< u64, User > > ) -> Self
+  {
+    Self { storage }
+  }
+}
+
+// it must be created with derive macro
+impl UserRepository for UserRepositoryDynamic
+{
+  fn add( &mut self, user : User ) -> Result< (), CanNotAddUser >
+  {
+    if self.storage.get( &user.id ).is_some()
+    {
+      return Err( CanNotAddUser );
+    }
+    Ok( self.storage.set( user.id, user ) )
+  }
+
+  fn get( &self, key : u64 ) -> Option< &User >
+  {
+    self.storage.get( &key )
+  }
+
+  fn update( &mut self, user : User )
+  {
+    self.storage.set( user.id, user )
+  }
+
+  fn remove( &mut self, key : u64 ) -> Option< User >
+  {
+    self.storage.remove( &key )
+  }
+}
+
+fn main() {}
+
+#[ cfg( test ) ]
+mod tests
+{
+  use super::*;
+
+  #[ test ]
+  fn static_repository()
+  {
+    let mut rep = UserRepositoryStatic{ storage : DB::default() };
+
+    let mut user = User{ id : 1, email : "user@gmail.com".into(), activated : true };
+    assert!( rep.add( user.clone() ).is_ok() );
+
+    assert_eq!( 1, rep.storage.storage.len() );
+    assert_eq!( Some( &user ), rep.get( 1 ) );
+
+    user.activated = false;
+    rep.update( user.clone() );
+
+    assert_eq!( Some( &user ), rep.get( 1 ) );
+
+    assert_eq!( Some( user ), rep.remove( 1 ) );
+    assert_eq!( 0, rep.storage.storage.len() );
+
+    assert_eq!( None, rep.get( 100 ) );
+    assert_eq!( None, rep.remove( 100 ) );
+  }
+
+  #[ test ]
+  fn dynamic_repository()
+  {
+    let mut rep = UserRepositoryDynamic{ storage : Box::new( DB::default() ) };
+
+    let mut user = User{ id : 1, email : "user@gmail.com".into(), activated : true };
+    assert!( rep.add( user.clone() ).is_ok() );
+
+    assert_eq!( Some( &user ), rep.get( 1 ) );
+
+    user.activated = false;
+    rep.update( user.clone() );
+
+    assert_eq!( Some( &user ), rep.get( 1 ) );
+    assert_eq!( Some( user ), rep.remove( 1 ) );
+
+    assert_eq!( None, rep.get( 100 ) );
+    assert_eq!( None, rep.remove( 100 ) );
+  }
 }
--- 1_concepts/1_7_sized/src/main.rs
+++ 1_concepts/1_7_sized/src/main.rs
@@ -1,4 +1,121 @@
-fn main()
+use step_1_6::*;
+
+trait Command {}
+
+trait CommandHandler< C : Command >
 {
-  println!( "Implement me!" );
+  type Context : ?Sized;
+  type Result;
+
+  fn handle_command( &self, cmd : &C, ctx : &mut Self::Context ) -> Self::Result;
+}
+
+struct Add;
+
+impl Command for Add {}
+
+impl CommandHandler< Add > for User
+{
+    type Context = dyn UserRepository;
+
+    type Result = Result< (), CanNotAddUser >;
+
+    fn handle_command( &self, _ : &Add, ctx : &mut Self::Context ) -> Self::Result
+    {
+      ctx.add( self.clone() )
+    }
+}
+
+struct Get;
+
+impl Command for Get {}
+
+impl CommandHandler< Get > for User
+{
+    type Context = dyn UserRepository;
+
+    type Result = Option< User >;
+
+    fn handle_command( &self, _ : &Get, ctx : &mut Self::Context ) -> Self::Result
+    {
+      ctx.get( self.id ).cloned()
+    }
+}
+
+struct Update;
+
+impl Command for Update {}
+
+impl CommandHandler< Update > for User
+{
+    type Context = dyn UserRepository;
+
+    type Result = ();
+
+    fn handle_command( &self, _ : &Update, ctx : &mut Self::Context ) -> Self::Result
+    {
+      ctx.update( self.clone() )
+    }
+}
+
+struct Remove;
+
+impl Command for Remove {}
+
+impl CommandHandler< Remove > for User
+{
+    type Context = dyn UserRepository;
+
+    type Result = Option< User >;
+
+    fn handle_command( &self, _ : &Remove, ctx : &mut Self::Context ) -> Self::Result
+    {
+      ctx.remove( self.id )
+    }
+}
+
+fn main() {}
+
+#[ cfg( test ) ]
+mod tests
+{
+  use super::*;
+
+  #[ test ]
+  fn static_repository()
+  {
+    let mut user = User{ id : 1, email : "user@gmail.com".into(), activated : true };
+    let mut rep = UserRepositoryStatic::new( DB::default() );
+
+    assert_eq!( None, user.handle_command( &Get, &mut rep) );
+
+    assert!( user.handle_command( &Add, &mut rep).is_ok() );
+    assert_eq!( Some( user.clone() ), user.handle_command( &Get, &mut rep) );
+
+    user.activated = false;
+    user.handle_command( &Update, &mut rep);
+    assert_eq!( Some( user.clone() ), user.handle_command( &Get, &mut rep) );
+
+    assert!( user.handle_command( &Remove, &mut rep).is_some() );
+    assert_eq!( None, user.handle_command( &Get, &mut rep) );
+  }
+
+  #[ test ]
+  fn dynamic_repository()
+  {
+    let mut user = User{ id : 1, email : "user@gmail.com".into(), activated : true };
+    let mut rep = UserRepositoryDynamic::new( Box::new( DB::default() ) );
+
+    assert_eq!( None, user.handle_command( &Get, &mut rep) );
+
+    assert!( user.handle_command( &Add, &mut rep).is_ok() );
+    assert_eq!( Some( user.clone() ), user.handle_command( &Get, &mut rep) );
+
+    user.activated = false;
+    user.handle_command( &Update, &mut rep);
+    assert_eq!( Some( user.clone() ), user.handle_command( &Get, &mut rep) );
+
+    assert!( user.handle_command( &Remove, &mut rep).is_some() );
+    assert_eq!( None, user.handle_command( &Get, &mut rep) );
+  }
 }
